9fa7591d6c047ea1e4c3e24d0d7717f96bbc9bb3
SubVray <jim.alberto.21@gmail.com>
2022-07-26 13:43:28 -0600

    slide veterinaria

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
