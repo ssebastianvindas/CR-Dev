0a20964e01382a7230542febd6027eb59e9c2ef2
drcoto <106129878+drcoto@users.noreply.github.com>
2022-07-31 22:44:35 -0600

    nickname

---
+++ js/list-pagos.js
@@ -0,0 +1,39 @@
+const inputs = document.querySelectorAll("input");
+const modal = document.getElementById("modal-expediente");
+const btnRol = document.getElementsByClassName("btn-ver");
+const span = document.getElementsByClassName("close")[0];
+let nickname = document.querySelector('.nickname');
+
+let closeModal = () => {
+    modal.style.display = "none";
+}
+
+let displayModal = () => {
+    modal.style.display = "block";
+}
+
+for (const ver of btnRol) {
+    ver.addEventListener('click', displayModal);
+}
+
+span.addEventListener('click', closeModal);
+
+let cambiarNickname = () => {
+    let usuarioConectado = JSON.parse(localStorage.getItem("usuarioConectado"));
+    switch (usuarioConectado.rol) {
+        case 1:
+            nickname.textContent = 'Administrador'
+            break;
+        case 2:
+            nickname.textContent = 'Secretaria'
+            break;
+        case 3:
+            nickname.textContent = 'Veterinario'
+            break;
+        case 4:
+            nickname.textContent = 'Cliente'
+            break;
+    }
+}
+
+window.addEventListener('load', cambiarNickname)
