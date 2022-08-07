add0b63f0a310186253aa6da87f8231880d98aad
drcoto <106129878+drcoto@users.noreply.github.com>
2022-08-07 17:55:58 -0600

    factura

---
+++ js/factura-enviar.js
@@ -0,0 +1,12 @@
+'use strict';
+const btnImprimir = document.querySelector('#btn-factura-imprimir');
+const btnEnviar = document.querySelector('#btn-factura-enviar');
+
+
+let enviarFactura = () => {
+
+};
+
+let imprimirFactura = () => {
+    window.print();
+};
