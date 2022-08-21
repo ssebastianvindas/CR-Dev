850ce6ebfea557c36dd58f5b4d9d6edb2e92853b
SubVray <jim.alberto.21@gmail.com>
2022-08-21 10:19:17 -0600

    update tablas
    cambio general en la tablas y en los listados

--- js/citas-listar.js
+++ js/citas-listar.js
@@ -1,270 +1,250 @@
-const cuerpoTabla = document.querySelector('#tbl-users tbody');
-const cuerpoTablaPendiente = document.querySelector('#tbl-pendientes tbody');
-const cuerpoTablaDoctor = document.querySelector('#tbl-doctor tbody');
-const seleccion = document.getElementById('cita-listar-doctores');
-const selectFiltroTabla = document.getElementById('cita-listar-doctores');
+const cuerpoTabla = document.querySelector("#tbl-users tbody");
+const cuerpoTablaPendiente = document.querySelector("#tbl-pendientes tbody");
+const cuerpoTablaDoctor = document.querySelector("#tbl-doctor tbody");
+const seleccion = document.getElementById("cita-listar-doctores");
+const selectFiltroTabla = document.getElementById("cita-listar-doctores");
 let citas = [];
 
-const llenarCitas = async() => {
+const llenarCitas = async () => {
+  citas = await getDatos("obtener-citas");
 
-    citas = await getDatos('obtener-citas');
-
-
-    llenarTablaDia();
-    llenarTablaDoctores();
-    llenarTablaPendientes();
-
-
+  llenarTablaDia();
+  llenarTablaDoctores();
+  llenarTablaPendientes();
 };
 
 const llenarTablaDia = () => {
-    //Limpia el contenido que tiene el cuerpo de la tabla.
-    cuerpoTabla.innerHTML = '';
-    //Para cada usuario que se encuentre dentro de la coleccion de usuarios
-    citas.forEach(cita => {
+  //Limpia el contenido que tiene el cuerpo de la tabla.
+  cuerpoTabla.innerHTML = "";
+  //Para cada usuario que se encuentre dentro de la coleccion de usuarios
+  citas.forEach((cita) => {
+    let fila = cuerpoTabla.insertRow();
 
-        let fila = cuerpoTabla.insertRow();
+    fila.insertCell().textContent = moment(cita.fecha).format("DD-MM-YYYY");
+    fila.insertCell().textContent = cita.hora;
+    fila.insertCell().textContent = cita.nombremascota;
+    fila.insertCell().textContent = cita.nombreduenno;
+    fila.insertCell().textContent = cita.procedimiento;
+    fila.insertCell().textContent = cita.doctor;
+    fila.insertCell().textContent = cita.estado;
 
-        fila.insertCell().textContent = moment(cita.fecha).format('DD-MM-YYYY');
-        fila.insertCell().textContent = cita.hora;
-        fila.insertCell().textContent = cita.nombremascota;
-        fila.insertCell().textContent = cita.nombreduenno;
-        fila.insertCell().textContent = cita.procedimiento;
-        fila.insertCell().textContent = cita.doctor;
-        fila.insertCell().textContent = cita.estado;
+    // Creación de la celda para los botones
+    let tdAcciones = fila.insertCell();
+
+    //Creación del boton editar
+    let btnEditar = document.createElement("button");
+    btnEditar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+    btnEditar.type = "button";
+    btnEditar.classList.add("btn-editar");
 
 
-        // Creación de la celda para los botones
-        let tdAcciones = fila.insertCell();
+    //Creación del botón de eliminar
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    btnEliminar.type = "button";
+    btnEliminar.classList.add("btn-eliminar");
 
-        //Creación del boton editar
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'Editar';
-        btnEditar.type = 'button';
-        btnEditar.classList.add('btn-editar');
-        //Creacion del boton eliminar
-        let btnEliminar = document.createElement('button');
-        btnEliminar.textContent = 'Cancelar';
-        btnEliminar.type = 'button';
-        btnEliminar.classList.add('btn-eliminar');
+    //Agregar el boton de editar a la celda acciones
+    tdAcciones.appendChild(btnEditar);
 
-        //Agregar el boton de editar a la celda acciones
-        tdAcciones.appendChild(btnEditar);
+    //Agregar el boton de eliminar a la celda acciones
+    tdAcciones.appendChild(btnEliminar);
 
-        //Agregar el boton de eliminar a la celda acciones
-        tdAcciones.appendChild(btnEliminar);
 
-        btnEliminar.addEventListener('click', () => {
-            Swal.fire({
-                title: 'Está seguro que desea cancelar la cita?',
-                text: "La acción no se puede revertir!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, eliminar!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    eliminarDatos('eliminar-cita', cita._id);
-
-                }
-            })
-        });
-
+    btnEliminar.addEventListener("click", () => {
+      Swal.fire({
+        title: "Está seguro que desea cancelar la cita?",
+        text: "La acción no se puede revertir!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminar!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          eliminarDatos("eliminar-cita", cita._id);
+        }
+      });
     });
+  });
 };
 const llenarTablaDoctor = () => {
-    //Limpia el contenido que tiene el cuerpo de la tabla.
-    cuerpoTablaDoctor.innerHTML = '';
-    //Para cada usuario que se encuentre dentro de la coleccion de usuarios
-    citas.forEach(cita => {
-        if (cita.doctor == seleccion.value) {
-            let fila = cuerpoTablaDoctor.insertRow();
+  //Limpia el contenido que tiene el cuerpo de la tabla.
+  cuerpoTablaDoctor.innerHTML = "";
+  //Para cada usuario que se encuentre dentro de la coleccion de usuarios
+  citas.forEach((cita) => {
+    if (cita.doctor == seleccion.value) {
+      let fila = cuerpoTablaDoctor.insertRow();
 
-            fila.insertCell().textContent = moment(cita.fecha).format('DD-MM-YYYY');
-            fila.insertCell().textContent = cita.hora;
-            fila.insertCell().textContent = cita.nombremascota;
-            fila.insertCell().textContent = cita.nombreduenno;
-            fila.insertCell().textContent = cita.procedimiento;
-            fila.insertCell().textContent = cita.doctor;
-            fila.insertCell().textContent = cita.estado;
+      fila.insertCell().textContent = moment(cita.fecha).format("DD-MM-YYYY");
+      fila.insertCell().textContent = cita.hora;
+      fila.insertCell().textContent = cita.nombremascota;
+      fila.insertCell().textContent = cita.nombreduenno;
+      fila.insertCell().textContent = cita.procedimiento;
+      fila.insertCell().textContent = cita.doctor;
+      fila.insertCell().textContent = cita.estado;
 
+      // Creación de la celda para los botones
+      let tdAcciones = fila.insertCell();
 
-            // Creación de la celda para los botones
-            let tdAcciones = fila.insertCell();
+      let btnEditar = document.createElement("button");
+      btnEditar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+      btnEditar.type = "button";
+      btnEditar.classList.add("btn-editar");
+  
+      //Creación del botón de eliminar
+      let btnEliminar = document.createElement("button");
+      btnEliminar.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+      btnEliminar.type = "button";
+      btnEliminar.classList.add("btn-eliminar");
 
-            //Creación del boton editar
-            let btnEditar = document.createElement('button');
-            btnEditar.textContent = 'Editar';
-            btnEditar.type = 'button';
-            btnEditar.classList.add('btn-editar');
-            //Creacion del boton eliminar
-            let btnEliminar = document.createElement('button');
-            btnEliminar.textContent = 'Cancelar';
-            btnEliminar.type = 'button';
-            btnEliminar.classList.add('btn-eliminar');
+      //Agregar el boton de editar a la celda acciones
+      tdAcciones.appendChild(btnEditar);
 
-            //Agregar el boton de editar a la celda acciones
-            tdAcciones.appendChild(btnEditar);
+      //Agregar el boton de eliminar a la celda acciones
+      tdAcciones.appendChild(btnEliminar);
 
-            //Agregar el boton de eliminar a la celda acciones
-            tdAcciones.appendChild(btnEliminar);
-
-
-            btnEliminar.addEventListener('click', () => {
-                Swal.fire({
-                    title: 'Está seguro que desea cancelar la cita?',
-                    text: "La acción no se puede revertir!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Sí, eliminar!'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        eliminarDatos('eliminar-cita', cita._id);
-                    }
-                })
-            });
-
-        }
-    });
+      btnEliminar.addEventListener("click", () => {
+        Swal.fire({
+          title: "Está seguro que desea cancelar la cita?",
+          text: "La acción no se puede revertir!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Sí, eliminar!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            eliminarDatos("eliminar-cita", cita._id);
+          }
+        });
+      });
+    }
+  });
 };
 
 const llenarTablaDoctores = () => {
-    //Limpia el contenido que tiene el cuerpo de la tabla.
-    cuerpoTablaDoctor.innerHTML = '';
-    //Para cada usuario que se encuentre dentro de la coleccion de usuarios
-    citas.forEach(cita => {
-        let fila = cuerpoTablaDoctor.insertRow();
-        fila.insertCell().textContent = moment(cita.fecha).format('DD-MM-YYYY');
-        fila.insertCell().textContent = cita.hora;
-        fila.insertCell().textContent = cita.nombremascota;
-        fila.insertCell().textContent = cita.nombreduenno;
-        fila.insertCell().textContent = cita.procedimiento;
-        fila.insertCell().textContent = cita.doctor;
-        fila.insertCell().textContent = cita.estado;
+  //Limpia el contenido que tiene el cuerpo de la tabla.
+  cuerpoTablaDoctor.innerHTML = "";
+  //Para cada usuario que se encuentre dentro de la coleccion de usuarios
+  citas.forEach((cita) => {
+    let fila = cuerpoTablaDoctor.insertRow();
+    fila.insertCell().textContent = moment(cita.fecha).format("DD-MM-YYYY");
+    fila.insertCell().textContent = cita.hora;
+    fila.insertCell().textContent = cita.nombremascota;
+    fila.insertCell().textContent = cita.nombreduenno;
+    fila.insertCell().textContent = cita.procedimiento;
+    fila.insertCell().textContent = cita.doctor;
+    fila.insertCell().textContent = cita.estado;
 
+    // Creación de la celda para los botones
+    let tdAcciones = fila.insertCell();
 
-        // Creación de la celda para los botones
-        let tdAcciones = fila.insertCell();
+    let btnEditar = document.createElement("button");
+    btnEditar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+    btnEditar.type = "button";
+    btnEditar.classList.add("btn-editar");
 
-        //Creación del boton editar
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'Editar';
-        btnEditar.type = 'button';
-        btnEditar.classList.add('btn-editar');
-        //Creacion del boton eliminar
-        let btnEliminar = document.createElement('button');
-        btnEliminar.textContent = 'Cancelar';
-        btnEliminar.type = 'button';
-        btnEliminar.classList.add('btn-eliminar');
+    //Creación del botón de eliminar
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    btnEliminar.type = "button";
+    btnEliminar.classList.add("btn-eliminar");
 
-        //Agregar el boton de editar a la celda acciones
-        tdAcciones.appendChild(btnEditar);
+    //Agregar el boton de editar a la celda acciones
+    tdAcciones.appendChild(btnEditar);
 
-        //Agregar el boton de eliminar a la celda acciones
-        tdAcciones.appendChild(btnEliminar);
+    //Agregar el boton de eliminar a la celda acciones
+    tdAcciones.appendChild(btnEliminar);
 
-
-        btnEliminar.addEventListener('click', () => {
-            Swal.fire({
-                title: 'Está seguro que desea cancelar la cita?',
-                text: "La acción no se puede revertir!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, eliminar!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    eliminarDatos('eliminar-cita', cita._id);
-                }
-            })
-        });
-
+    btnEliminar.addEventListener("click", () => {
+      Swal.fire({
+        title: "Está seguro que desea cancelar la cita?",
+        text: "La acción no se puede revertir!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminar!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          eliminarDatos("eliminar-cita", cita._id);
+        }
+      });
     });
+  });
 };
 
 const llenarTablaPendientes = () => {
-    //Limpia el contenido que tiene el cuerpo de la tabla.
-    cuerpoTablaPendiente.innerHTML = '';
-    //Para cada usuario que se encuentre dentro de la coleccion de usuarios
-    citas.forEach(cita => {
+  //Limpia el contenido que tiene el cuerpo de la tabla.
+  cuerpoTablaPendiente.innerHTML = "";
+  //Para cada usuario que se encuentre dentro de la coleccion de usuarios
+  citas.forEach((cita) => {
+    let fila = cuerpoTablaPendiente.insertRow();
 
-        let fila = cuerpoTablaPendiente.insertRow();
+    fila.insertCell().textContent = moment(cita.fecha).format("DD-MM-YYYY");
+    fila.insertCell().textContent = cita.hora;
+    fila.insertCell().textContent = cita.nombremascota;
+    fila.insertCell().textContent = cita.nombreduenno;
+    fila.insertCell().textContent = cita.procedimiento;
+    fila.insertCell().textContent = cita.doctor;
+    fila.insertCell().textContent = cita.estado;
+    //Creación del botón de editar
 
-        fila.insertCell().textContent = moment(cita.fecha).format('DD-MM-YYYY');
-        fila.insertCell().textContent = cita.hora;
-        fila.insertCell().textContent = cita.nombremascota;
-        fila.insertCell().textContent = cita.nombreduenno;
-        fila.insertCell().textContent = cita.procedimiento;
-        fila.insertCell().textContent = cita.doctor;
-        fila.insertCell().textContent = cita.estado;
+    // Creación de la celda para los botones
+    let tdAccionesPendiente = fila.insertCell();
 
+    //Creación del boton editar
+    let btnAgregar = document.createElement("button");
+    btnAgregar.textContent = 'Asignar';
+    btnAgregar.type = "button";
+    btnAgregar.classList.add("btn-agregar");
 
-        // Creación de la celda para los botones
-        let tdAccionesPendiente = fila.insertCell();
+    //Creación del botón de eliminar
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    btnEliminar.type = "button";
+    btnEliminar.classList.add("btn-eliminar");
 
-        //Creación del boton editar
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'Asignar';
-        btnEditar.type = 'button';
-        btnEditar.classList.add('btn-editar');
-        //Creacion del boton eliminar
-        let btnEliminar = document.createElement('button');
-        btnEliminar.textContent = 'Cancelar';
-        btnEliminar.type = 'button';
-        btnEliminar.classList.add('btn-eliminar');
+    //Agregar el boton de editar a la celda acciones
+    tdAccionesPendiente.appendChild(btnAgregar);
 
-        //Agregar el boton de editar a la celda acciones
-        tdAccionesPendiente.appendChild(btnEditar);
+    //Agregar el boton de eliminar a la celda acciones
+    tdAccionesPendiente.appendChild(btnEliminar);
 
-        //Agregar el boton de eliminar a la celda acciones
-        tdAccionesPendiente.appendChild(btnEliminar);
-
-
-        btnEliminar.addEventListener('click', () => {
-            Swal.fire({
-                title: 'Está seguro que desea cancelar la cita?',
-                text: "La acción no se puede revertir!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, eliminar!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    eliminarDatos('eliminar-cita', cita._id);
-                }
-            })
-        });
-
-        btnEditar.addEventListener('click', () => {
-            Swal.fire({
-                title: 'Está seguro que desea agendarse la cita?',
-                text: "La acción no se puede revertir!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, Asignar!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire(
-                        'Eliminado!',
-                        'La cita fue asignada.',
-                        'success'
-                    )
-                }
-            })
-        });
-
+    btnEliminar.addEventListener("click", () => {
+      Swal.fire({
+        title: "Está seguro que desea cancelar la cita?",
+        text: "La acción no se puede revertir!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminar!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          eliminarDatos("eliminar-cita", cita._id);
+        }
+      });
     });
+
+    btnEditar.addEventListener("click", () => {
+      Swal.fire({
+        title: "Está seguro que desea agendarse la cita?",
+        text: "La acción no se puede revertir!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, Asignar!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("Eliminado!", "La cita fue asignada.", "success");
+        }
+      });
+    });
+  });
 };
 
-
-
-
 llenarCitas();
-selectFiltroTabla.addEventListener('change', llenarTablaDoctor);
+selectFiltroTabla.addEventListener("change", llenarTablaDoctor);
--- js/list-citas-tablas.js
+++ js/list-citas-tablas.js
@@ -1,71 +1,62 @@
-const cuerpoTabla = document.querySelector('#tbl-users tbody');
+const cuerpoTabla = document.querySelector("#tbl-users tbody");
 let citas = [];
 let user = JSON.parse(localStorage.getItem("usuarioConectado"));
 
-const inicializarListaCitasByUser = async() => {
-    citas = await getDatosByUser('obtener-citas-usuario', user.usuario);
-    llenarTablaList();
+const inicializarListaCitasByUser = async () => {
+  citas = await getDatosByUser("obtener-citas-usuario", user.usuario);
+  llenarTablaList();
 };
 
+const llenarTablaList = async () => {
+  //Limpia el contenido que tiene el cuerpo de la tabla.
+  cuerpoTabla.innerHTML = "";
+  //Para cada usuario que se encuentre dentro de la coleccion de usuarios
+  citas.forEach((cita) => {
+    let fila = cuerpoTabla.insertRow();
+    fila.insertCell().textContent = cita.fecha;
+    fila.insertCell().textContent = cita.hora;
+    fila.insertCell().textContent = cita.nombreMascota;
+    fila.insertCell().textContent = cita.nombreOwner;
+    fila.insertCell().textContent = cita.procedimiento;
+    fila.insertCell().textContent = cita.doctor;
+    fila.insertCell().textContent = cita.estado;
 
+    // Creación de la celda para los botones
+    let tdAcciones = fila.insertCell();
 
-const llenarTablaList = async() => {
-    //Limpia el contenido que tiene el cuerpo de la tabla.
-    cuerpoTabla.innerHTML = "";
-    //Para cada usuario que se encuentre dentro de la coleccion de usuarios
-    citas.forEach((cita) => {
+    //Creación del boton calificar
+    let btnEditar = document.createElement("button");
+    btnEditar.textContent = "Calificar";
+    btnEditar.type = "button";
+    btnEditar.classList.add("btn-editar");
+    //Creación del botón de eliminar
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    btnEliminar.type = "button";
+    btnEliminar.classList.add("btn-eliminar");
 
-        let fila = cuerpoTabla.insertRow();
-        fila.insertCell().textContent = cita.fecha;
-        fila.insertCell().textContent = cita.hora;
-        fila.insertCell().textContent = cita.nombreMascota;
-        fila.insertCell().textContent = cita.nombreOwner;
-        fila.insertCell().textContent = cita.procedimiento;
-        fila.insertCell().textContent = cita.doctor;
-        fila.insertCell().textContent = cita.estado;
+    //Agregar el boton de editar a la celda acciones
+    tdAcciones.appendChild(btnEditar);
 
+    //Agregar el boton de eliminar a la celda acciones
+    tdAcciones.appendChild(btnEliminar);
 
-        // Creación de la celda para los botones
-        let tdAcciones = fila.insertCell();
-
-        //Creación del boton editar
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'Calificar Veterinario';
-        btnEditar.type = 'button';
-        btnEditar.classList.add('btn-editar');
-        //Creacion del boton eliminar
-        let btnEliminar = document.createElement('button');
-        btnEliminar.textContent = 'Cancelar';
-        btnEliminar.type = 'button';
-        btnEliminar.classList.add('btn-eliminar');
-
-        //Agregar el boton de editar a la celda acciones
-        tdAcciones.appendChild(btnEditar);
-
-        //Agregar el boton de eliminar a la celda acciones
-        tdAcciones.appendChild(btnEliminar);
-
-        btnEliminar.addEventListener('click', () => {
-            Swal.fire({
-                title: 'Está seguro que desea cancelar la cita?',
-                text: "La acción no se puede revertir!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, eliminar!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire(
-                        'Eliminado!',
-                        'La cita fue cancelada.',
-                        'success'
-                    )
-                }
-            })
-        });
-
+    btnEliminar.addEventListener("click", () => {
+      Swal.fire({
+        title: "Está seguro que desea cancelar la cita?",
+        text: "La acción no se puede revertir!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminar!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("Eliminado!", "La cita fue cancelada.", "success");
+        }
+      });
     });
+  });
 };
 
 inicializarListaCitasByUser();
